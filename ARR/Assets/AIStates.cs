09471fad101e741b05ffb4c263d43b40d681b7f7
Ghamanako <73056948+Ghamanako@users.noreply.github.com>
2021-08-26 21:16:22 +0700

    pload
    Update AR

---
+++ ARR/Assets/AIStates.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIStates
+{ 
+    Idle, Attack, Attack2, Walk, Walk_Back, Hit, Defend, NONE
+}
---
+++ ARR/Assets/Drag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    public bool isDragable = true;
+    public bool isDragged = false;
+
+    void Update()
+    {
+     if(isDragged)
+        {
+            transform.position = (Vector3) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (isDragable && Input.GetMouseButtonDown(0))
+        {
+            isDragged = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isDragged = false;
+    }
+}
