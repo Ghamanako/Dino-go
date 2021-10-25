73cf6fdc719834782a535f2ad622815f1f967f02
Ghamanako <73056948+Ghamanako@users.noreply.github.com>
2021-10-25 22:31:05 +0700

    beb

---
+++ ARR/Assets/Dino-Go/Script/AUDIOSetting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+public class AUDIOSetting : MonoBehaviour
+{
+    public AudioMixer BgmMixer;
+    
+    public Slider BGMSlider;
+
+    public void SetVolume(float volume)
+    {
+        BgmMixer.SetFloat("BgmVolume", volume);
+    }
+
+   
+
+    private void Start()
+    {
+        BGMSlider.value = PlayerPrefs.GetFloat("BgmVolume",0);
+       
+    }
+
+    private void OnDisable()
+    {
+        float BGMSistem = 0;
+        
+
+
+        BgmMixer.GetFloat("BgmVolume", out BGMSistem);
+       
+        PlayerPrefs.SetFloat("BgmVolume", BGMSistem);
+
+        PlayerPrefs.Save();
+    }
+}
