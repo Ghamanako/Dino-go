7b38aeb16874890ef76aa367791221e3a18861c6
Ghamanako <73056948+Ghamanako@users.noreply.github.com>
2021-10-25 08:34:27 +0700

    update lagi
    masih setting menu

---
+++ ARR/Assets/Dino-Go/Script/bukaPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bukaPanel : MonoBehaviour
+{
+    public GameObject panel, Next, Back;
+   
+    
+    public void BukaPanel()
+    {
+        panel.gameObject.SetActive(true);
+    }
+
+    public void TutupPanel()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    public void TutorialPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void nextPanel()
+    {
+        Next.SetActive(true);
+        Back.SetActive(false);
+    }
+    public void BAckPanel()
+    {
+        Next.SetActive(false);
+        Back.SetActive(true);
+    }
+
+}
